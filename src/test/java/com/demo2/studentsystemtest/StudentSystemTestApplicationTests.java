d203367afc0bbebbe2d2e1ecb004beb41a2f581d
dotun <dotun.ogundare@gmail.com>
2022-02-10 12:28:27 +0100

    Initial commit

---
+++ src/test/java/com/demo2/studentsystemtest/StudentSystemTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo2.studentsystemtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentSystemTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
